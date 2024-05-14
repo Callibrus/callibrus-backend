a2d31d4400530cb28fafd1d24cb1ab62981d72e5
AndrewPopesku <andepopesku@gmail.com>
2024-05-14 22:22:42 +0300

    added new endpoints for BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,10 +1,11 @@
 using Callibrus.Server.Data;
+using Callibrus.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Callibrus.Server.Controllers;
 
-[Route("api/[controller]")]
+[Route("api")]
 [ApiController]
 public class BookController : ControllerBase
 {
@@ -15,10 +16,61 @@ public class BookController : ControllerBase
         _libraryDbContext = libraryDbContext;
     }
     
-    [HttpGet]
+    [HttpGet("books")]
     public async Task<IActionResult> GetBooks()
     {
-        var books = _libraryDbContext.Books;
+        var books = _libraryDbContext.Books
+            .Include(b => b.Authors);
         return Ok(await books.ToListAsync());
+    }
+    
+    [HttpGet("book/{id}")]
+    public async Task<IActionResult> GetBookById(int id)
+    {
+        var book = await _libraryDbContext.Books
+            .Include(b => b.Authors)
+            .FirstOrDefaultAsync(b => b.Id == id);
+        
+        if (book == null)
+            return NotFound();
+        
+        return Ok(book);
+    }
+    
+    [HttpPut("book/{id}")]
+    public async Task<IActionResult> UpdateBook(int id, [FromBody] Book bookFromBody)
+    {
+        var book = await _libraryDbContext.Books
+            .Include(b => b.Authors)
+            .FirstOrDefaultAsync(b => b.Id == id);
+        if (book == null)
+        {
+            return NotFound($"Book with ID {id} not found.");
+        }
+
+        try
+        {
+            _libraryDbContext.Books.Update(book);
+            await _libraryDbContext.SaveChangesAsync();
+            return NoContent();
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "Unable to save changes: " + ex.Message);
+        }
+    }
+    
+    [HttpDelete("book/{id}")]
+    public async Task<IActionResult> DeleteBook(int id)
+    {
+        var book = await _libraryDbContext.Books.FindAsync(id);
+        if (book == null)
+        {
+            return NotFound($"Book with ID {id} not found.");
+        }
+
+        _libraryDbContext.Books.Remove(book);
+        await _libraryDbContext.SaveChangesAsync();
+        return NoContent();
     }
 }
