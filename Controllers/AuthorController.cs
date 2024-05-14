f6d3ca25d61400bec5f2017f3ac1401fcfc3bf77
Tetiana <sobkotiana@gmail.com>
2024-05-14 22:38:00 +0300

    Added AuthorControler with CRUD methods

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,76 @@
+using Callibrus.Server.Data;
+using Callibrus.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Callibrus.Server.Controllers;
+
+[Route("api")]
+[ApiController]
+public class AuthorController : ControllerBase
+{
+    private readonly LibraryDbContext _libraryDbContext;
+
+    public AuthorController(LibraryDbContext libraryDbContext)
+    {
+        _libraryDbContext = libraryDbContext;
+    }
+    
+    [HttpGet("authors")]
+    public async Task<IActionResult> GetAuthors()
+    {
+        var authors = _libraryDbContext.Authors
+            .Include(b => b.Books);
+        return Ok(await authors.ToListAsync());
+    }
+    
+    [HttpGet("author/{id}")]
+    public async Task<IActionResult> GetAuthorById(int id)
+    {
+        var author = await _libraryDbContext.Authors
+            .Include(b => b.Books)
+            .FirstOrDefaultAsync(b => b.Id == id);
+        
+        if (author == null)
+            return NotFound();
+        
+        return Ok(author);
+    }
+    
+    [HttpPut("author/{id}")]
+    public async Task<IActionResult> UpdateAuthor(int id, [FromBody] Author authorFromBody)
+    {
+        var author = await _libraryDbContext.Authors
+            .Include(b => b.Books)
+            .FirstOrDefaultAsync(b => b.Id == id);
+        if (author == null)
+        {
+            return NotFound($"Author with ID {id} not found.");
+        }
+
+        try
+        {
+            _libraryDbContext.Authors.Update(author);
+            await _libraryDbContext.SaveChangesAsync();
+            return NoContent();
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "Unable to save changes: " + ex.Message);
+        }
+    }
+    
+    [HttpDelete("author/{id}")]
+    public async Task<IActionResult> DeleteAuthor(int id)
+    {
+        var author = await _libraryDbContext.Authors.FindAsync(id);
+        if (author == null)
+        {
+            return NotFound($"Author with ID {id} not found.");
+        }
+
+        _libraryDbContext.Authors.Remove(author);
+        await _libraryDbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
